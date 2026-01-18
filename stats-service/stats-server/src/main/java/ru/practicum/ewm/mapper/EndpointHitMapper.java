79739e735a854d523ef754dcb885b145b4293441
abdras <aabdras@yandex.kz>
2026-01-18 18:49:41 +0500

    diploma project, 2 step

--- stats-service/stats-server/src/main/java/ru/practicum/ewm/mapper/EndpointHitMapper.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/mapper/EndpointHitMapper.java
@@ -4,18 +4,13 @@ import ru.practicum.ewm.model.EndpointHit;
 import org.springframework.stereotype.Component;
 import ru.practicum.ewm.dto.EndpointHitDto;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @Component
 public class EndpointHitMapper {
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
     public EndpointHitDto toEndpointHitDto(EndpointHit endpointHit) {
         return new EndpointHitDto(endpointHit.getApp(),
                 endpointHit.getUri(),
                 endpointHit.getIp(),
-                endpointHit.getTimestamp().format(formatter)
+                endpointHit.getTimestamp()
         );
     }
 
@@ -24,7 +19,7 @@ public class EndpointHitMapper {
                 endpointHitDto.getApp(),
                 endpointHitDto.getUri(),
                 endpointHitDto.getIp(),
-                LocalDateTime.parse(endpointHitDto.getTimestamp(), formatter)
+                endpointHitDto.getTimestamp()
         );
     }
 }
