8236a633c6480e4de85bcab129565da032101c1d
abdras <aabdras@yandex.kz>
2026-01-20 21:53:16 +0500

    diploma project, 2 step

--- stats-service/stats-server/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.repository;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.model.EndpointHit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
